f50af3f9d00381d4a4067823a716c6b997f61441
Bisma-Waseem <bwartofficial88@gmail.com>
2024-10-12 22:02:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,205 @@
+// //------ chap 1----------
+
+// // question 1 
+// alert ("Salaam, Welcome to this page");
+
+// // question 2
+// alert("Error! Entr a valid password");
+
+// // question 3
+// alert("wecome\n to this website");
+
+
+// // question 4
+// alert("used JavaScript");
+// alert("enjoy...");
+
+// alert("SALAAM \n hi");
+// console.log("hi");
+
+// -------chap 2--------
+// Q 1
+// let username = "bisma";
+// console.log(username);
+
+// Q 2
+//  let myName;
+// myName= ("bisma");
+// console.log(myName);
+
+
+// Q 3
+// let message;
+// message = "hello world";
+// alert(message);
+
+// // Q 4
+// // let student bio data
+// let sname="bisma";
+// let sage ="22"
+// alert(`student name is ${sname} and student age is ${sage}`);
+
+// // Q 5
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// // Q 6
+// let myemail = "bwartofficial88@gmail.com"
+// alert("my email is " + myemail);
+
+// // Q 7
+// let bookname = "A Smart Way To Learn Javascript";
+// alert("I am trying to learn" + bookname);
+
+// // Q 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// // Q 9
+// let line="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(line);
+
+// --------CHAP 3---------
+// Q 1
+// let age ="22 years old";
+// alert("I am" + age);
+// Q 2
+// let vistedTime= 14;
+// alert("You have visted this site"+ vistedTime);
+
+// Q 3
+// var birthyear = "2002";
+// document.write("My birth year is" +birthyear// );
+
+// Q 4
+// var numbers = 10;
+// var order = 5;
+// var company = "xyz";
+// document.write(`ali visted ${numbers} times this site and orderd ${order} shirts on ${company}`);
+
+
+// ----------CHAP 4-----------
+
+// Q 1
+// var one=1
+// var two=2
+// var three=3
+// console.log(one + two + three);
+
+// Q 2
+// legal var
+// let myPhone="android"
+// let my_phone="android"
+// let MyPhone = "android"
+// let phone123="android"
+// let num$=1234
+
+// ILLEGAL VAR
+// let myPhone.="android"
+// let my_-phone="android"
+// let My h0ne = "android"
+// let phone@="android"
+// let num--=1234
+// if (5>4) {console.log(
+// "javascipt")
+// }
+// // 2
+// if (5>=4) {console.log("html");
+// }
+// // 3
+// if (true) {console.log("css");
+// }
+// // 4
+// if (2*2 ==4) {console.log("phython");    
+// }
+// // 5
+// if (2+2 ==4) {console.log("salaam");
+
+// }
+// illegal var
+// 1
+
+// if (5>9) {console.log(
+//     "javascipt")
+//     }
+//     // 2
+//     if (5>=8) {console.log("html");
+//     }
+//     // 3
+//     if (false) {console.log("css");
+//     }
+//     // 4
+//     if (2*2 ==6) {console.log("phython");    
+//     }
+//     // 5
+//     if (2+2 ==5) {console.log("salaam");
+
+//      }
+// document.write(`<h1>Rules for naming JS variable<h1>`);
+// document.write("<br>");
+// document.write("variable can only contain,numbers,$ and leter. For example: $my_1stVariable") 
+// document.write("<br>");
+// document.write("variable names are sensitive ") 
+// document.write("<br>");
+// document.write("variable names should not JS keywords")
+
+
+// ----------CHAP 5-------
+// Q 1
+//  var num1 = 5;
+// var num2 = 3;
+// document.write(`sum of ${num1} and ${num2} is ${num1+num2}`);
+
+// Q 2
+//  var num1 = 5;
+//  var num2 = 3;
+// document.write(num1 - num2);
+// multiply
+// document.write(num1 * num2);
+// division
+//  document.write(num1 / num2);
+
+// Q 3
+// let value=""
+// document.write("variable after variable decleration is" + value + "<br>")
+// value=8
+// document.write("initial value:" + value + "<br>")
+// document.write(`value after increment is :${++value} <br>` 
+// );
+// document.write(`value after addition : ${value+7} <br>`)
+// value=16
+// document.write(`value after decrement: ${--value} <br>`)
+// document.write(`The remainder is :${value/3}`)
+
+// Q 4
+// let price=600
+// document.write(`Total cost to buy 5 tickrts is ${price*5}PKR <br>`)
+
+// // Q 5
+// let num=4
+// for (var bb=1; bb<=10; bb++){
+//     let answer=num*bb
+// console.log
+// (`${num} *${num} = ${num*bb}`);
+// }
+// // MARK SHEET
+// let totalmarks=900
+// let obtaines = 804
+// document.write(`percentage ${obtaines/totalmarks *100} `)
+
+// SHOPING CART
+// let price1=650
+// let price2=100
+// let quantityprice1=3
+// let quantityprice2=7
+// let charges=100
+// document.write(`total cost of your order is ${price1*quantityprice1 + price2*quantityprice2 + charges}`)
+
+// Q 10
+// let a=0
+// document.write(`${a+5 *10 /2}`)
+// Q 13
+// let fsnack = "chocolate chip";
+// let cage=15
+// let mage=65
+// let amount=3
+// document.write(`favorite snack:chocolate chips <br> Current age is:${cage} <br> Estimated maximum age ${mage} <br> Amount of snack per day: ${amount} <br> You will need ${cage+mage+amount} chocolate chpis to last you until ripe old age of 65`)
+// 
